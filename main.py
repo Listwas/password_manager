fdb93de7b89deffa3cbd2610c4e2f07c5a2451d1
Listwas <listwa.patrykkr@gmail.com>
2025-11-21 15:16:33 +0100

    feat: choice menu, remove entry, add entry, help, quit

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import os
 class Vault:
     def __init__(self, vault_login):
         self.vault_login = vault_login
-
-        print("\033[2J\033[H")
-        self.login(vault_login)
-        self.choice_menu(vault_login)
+        welcome = self.login(vault_login)
+        while True:
+            self.choice_menu(vault_login, welcome)
 
     def login(self, vault_login):
         if not os.path.isdir("vault"):
@@ -16,10 +15,13 @@ class Vault:
         if not os.path.isfile(f"vault/{vault_login}.vault"):
             open(f"vault/{vault_login}.vault", "w")
             print(f"created new user: {vault_login}")
+            return "    hello," 
+        else: 
+            return "    hello again,"
 
-    def choice_menu(self, vault_login, phraze="", tries=3):
+    def choice_menu(self, vault_login, welcome, phraze="", tries=3):
         print("--- Password manager ---")
-        print(f"    hello, {vault_login}") 
+        print(f"{welcome} {vault_login}")
         print("\n    choose option\n"
               "    1 add entry\n"
               "    2 read entry\n"
@@ -28,22 +30,33 @@ class Vault:
               "    5 quit\n")
         try:
             choice = int(input(f"--- option: "))
+
+            self.match_choice(choice)
         except ValueError:
             print("\033[2J\033[H")
             phraze = f"option should be a number\n     {tries} tries left\n"
             if tries >= 1:
                 print(phraze)
-                self.choice_menu(vault_login, phraze, tries-1)
-            quit()
+                self.choice_menu(vault_login, "    hello", phraze, tries-1)
+                
+    def match_choice(self, choice):
         match choice:
             case 1:
                 website, username, password = self.entry_info()                
-                self.add_entry(vault_login, website, username, password )
+                self.add_entry(vault_login, website, username, password)
             case 2:
+                print("\033[2J\033[H")
                 self.read_entry(vault_login)
-            case _:
-                return 0
-            
+            case 3:
+                print("\033[2J\033[H")
+                self.remove_entry(vault_login)
+            case 4:
+                self.help()
+            case 5:
+                quit()
+            case _: 
+                print("asd")
+
     def entry_info(self):
         website = input("    website: ").strip()
         username = input("    username: ").strip()
@@ -53,6 +66,9 @@ class Vault:
     def add_entry(self, vault_login, website, username, password):
         with open(f"vault/{vault_login}.vault", "a") as f:
             f.write(f"{website} {username} {password}\n")
+
+            print("\033[2J\033[H")
+            print(f"added entry: {website} {username} {password[: -4]}***** ")
             f.close()
 
     def read_entry(self, vault_login):
@@ -60,12 +76,34 @@ class Vault:
         with open(f"vault/{vault_login}.vault", "r") as f:
             for index, line in enumerate(f):
                 print(f"    {index}: {line}")
+            f.close()
         # fix this later        
-            
+
+    def remove_entry(self, vault_login):
+        with open(f"vault/{vault_login}.vault", "r") as f:
+            self.read_entry(vault_login)
+            try:
+                remove = int(input("    delete entry: "))
+                # fix this later
+            except ValueError:
+                print("\033[2J\033[H")
+                print("    not a number, try again")
+
+
+
+    def help(self):
+        print("\033[2J\033[H")
+        print("    1. add an entry by providing: website username password\n "
+              "   2. read all your passwords\n"
+              "    3. remove entry by typing it's index number\n"
+              "    4. display this menu\n"
+              "    5. exit the program\n")
+
 if __name__ == "__main__":
+    print("\033[2J\033[H")
     print("--- Password manager ---")
     vault_login = input("    username: ")
+
+    print("\033[2J\033[H")
     Vault(vault_login)
 
-# later
-# add different msg for returning user
